78ddfdc5264baa4021e57035cc6fc1ea7e7ac774
alejandrogzi <jose.gonzalesdezavala1@unmsm.edu.pe>
2023-10-07 02:05:36 -0500

    BREAKING CHANGE: TogaDir impl; STEP 1 base features

--- postoga.py
+++ postoga.py
@@ -21,293 +21,172 @@ import time
 from constants import Constants
 from version import __version__
 from logger import Log
-
+from modules.utils import shell, bed_reader
+from modules.convert_from_bed import bed_to_gtf, bed_to_gff
+from modules.make_query_table import query_table
+from modules.write_isoforms import isoform_writer
+from modules.assembly_stats import qual_by_ancestral
 
 __author__ = "Alejandro Gonzales-Irribarren"
-__version__ = __version__
 __email__ = "jose.gonzalesdezavala1@unmsm.edu.pe"
 __github__ = "https://github.com/alejandrogzi"
+__version__ = "0.4.0-devel"
 
 
-log = None
+class TogaDir:
+    """A class to represent a TOGA results directory."""
 
+    def __init__(self, args: argparse.Namespace) -> None:
+        """
+        Constructs all the necessary attributes for the TogaDir object.
 
-def toga_table(args: argparse.Namespace) -> pd.DataFrame:
-    """
-    Return a pandas DataFrame with all projections and metadata.
+        @type args: argparse.Namespace
+        @param args: defined arguments
+        """
 
-    @type args: subprocess.Namespace
-    @param args: defined arguments
-    @rtype: DataFrame
-    @return a DataFrame with all the projections and metada
-    """
+        self.log = Log(args.path, Constants.FileNames.LOG)
+        self.mode = args.mode
+        self.args = args
 
-    global log
+        if args.mode != "haplotype":
+            """The default branch of postoga"""
+            ##### STEP 1 #####
+            self.path = args.path
+            self.to = args.to
+            self.q_assembly = args.assembly_qual
 
-    # Reads orthology_classification, loss_sum_data, and ortholog_scores.
-    orthology = pd.read_csv(
-        os.path.join(args.path, Constants.FileNames.ORTHOLOGY), sep="\t"
-    )
-    loss = pd.read_csv(
-        os.path.join(args.path, Constants.FileNames.CLASS),
-        sep="\t",
-        header=None,
-        names=["projection", "transcript", "class"],
-    )
-    score = pd.read_csv(os.path.join(args.path, Constants.FileNames.SCORES), sep="\t")
-    isoforms = pd.read_csv(
-        os.path.join(args.path, Constants.FileNames.ISOFORMS), sep="\t", header=None
-    )
-    quality = pd.read_csv(
-        os.path.join(args.path, Constants.FileNames.QUALITY), sep="\t"
-    )
+            if args.by_class:
+                self.by_class = args.by_class
+            if args.by_rel:
+                self.by_rel = args.by_rel
+            if args.threshold:
+                self.threshold = args.threshold
 
-    # Creates a dictionary: transcript -> gene
-    isoforms_dict = isoforms.set_index(1).to_dict().get(0)
+        else:
+            """The haplotype branch of postoga"""
+            self.paths = args.haplotype_path.split(",")
+            self.rule = args.rule.split(">")
 
-    # Subsets loss to consider only projections
-    loss = loss[loss["projection"] == "PROJECTION"]
-    loss["helper"] = loss["transcript"].str.split(".").str[0]
+    def filter_bed(self) -> str:
+        """Filters the original .bed file to produce a custom filtered file"""
 
-    ortho_x_loss = pd.merge(
-        orthology, loss, left_on="q_transcript", right_on="transcript", how="outer"
-    )
+        initial = len(self.table)
 
-    ortho_x_loss["helper"].fillna(ortho_x_loss["t_gene"], inplace=True)
+        if self.threshold:
+            table = self.table[self.table["pred"] >= float(self.threshold)]
+            self.log.record(
+                f"discarded {initial - len(table)} projections with orthology scores <{self.threshold}"
+            )
 
-    # Merge transcript names (under the column "gene") and chain IDs (under the column "chain")
-    score["transcripts"] = score["gene"] + "." + score["chain"].astype(str)
-    score = score[["transcripts", "pred"]]
+        if self.by_class:
+            edge = len(table)
+            table = table[table["class"].isin(self.by_class.split(","))]
+            self.log.record(
+                f"discarded {edge - len(table)} projections with classes other than {self.by_class}"
+            )
 
-    table = pd.merge(ortho_x_loss, score, left_on="transcript", right_on="transcripts")
+        if self.by_rel:
+            edge = len(table)
+            table = table[table["relation"].isin(self.by_rel.split(","))]
+            self.log.record(
+                f"discarded {edge - len(table)} projections with relationships other than {self.by_rel}"
+            )
 
-    # Create a new column with a rename orthology relationship
-    table["relation"] = table["orthology_class"].map(Constants.ORTHOLOGY_TYPE)
-    table["t_gene"].fillna(table["helper"].map(isoforms_dict), inplace=True)
+        # Read the original .bed file and filter it based on the transcripts table
+        bed = pd.read_csv(
+            os.path.join(self.path, Constants.FileNames.BED), sep="\t", header=None
+        )
+        bed = bed[bed[3].isin(table["transcripts"])]
+        custom_table = table[table["transcripts"].isin(bed[3])]
 
-    # Merge quality data
-    table = pd.merge(table, quality, left_on="transcripts", right_on="Projection_ID")
+        # Write the filtered .bed file
+        f = os.path.join(self.path, Constants.FileNames.FILTERED_BED)
+        bed.to_csv(
+            f,
+            sep="\t",
+            header=None,
+            index=False,
+        )
 
-    table = table[
-        [
-            "t_gene",
-            "helper",
-            "transcripts",
-            "relation",
-            "class",
-            "pred",
-            "q_gene",
-            "confidence_level",
+        info = [
+            f"kept {len(bed)} projections after filters, discarded {initial - len(bed)}.",
+            f"{len(bed)} projections are coming from {len(custom_table['helper'].unique())} unique transcripts and {len(custom_table['t_gene'].unique())} genes",
+            f"class stats of new bed: {custom_table['class'].value_counts().to_dict()}",
+            f"relation stats of new bed: {custom_table['relation'].value_counts().to_dict()}",
+            f"confidence stats of new bed: {custom_table['confidence_level'].value_counts().to_dict()}",
+            f"filtered bed file written to {f}",
         ]
-    ]
 
-    log.record(
-        f"found {len(table)} projections, {len(table['helper'].unique())} unique transcripts, {len(table['t_gene'].unique())} unique genes"
-    )
-    log.record(f"class stats: {table['class'].value_counts().to_dict()}")
-    log.record(f"relation stats: {table['relation'].value_counts().to_dict()}")
-    log.record(
-        f"confidence stats: {table['confidence_level'].value_counts().to_dict()}"
-    )
+        [self.log.record(i) for i in info]
 
-    return table
+        return f
 
+    def get_haplotype_classes(self) -> None:
+        """
+        @type paths: str
+        @param paths: comma separated paths to TOGA results directories
 
-def write_isoforms(args: argparse.Namespace, table: pd.DataFrame) -> str:
-    """
-    Writes all isoforms to a text file
+        cmd: postoga -m haplotypes -hpath path1,path2,path3
+        """
+        paths = self.paths
+        dfs = []
 
-    @type args: subprocess.Namespace
-    @param args: defined arguments
-    @type table: pd.DataFrame
-    @param table: a pandas DataFrame
-    """
+        # For each path build a query table and filtered based on queries annotated
+        for path in paths:
+            table = query_table(path)
+            bed = bed_reader(path)
+            df = table[table["transcripts"].isin(bed[3])]
+            dfs.append(df)
 
-    global log
+        return dfs
 
-    f = os.path.join(args.path, Constants.FileNames.OWNED_ISOFORMS)
+    def run(self) -> None:
+        """
+        The postoga runner function
 
-    # Get only gene:transcript pairs
-    table = table.iloc[:, [0, 2]]
-    table.to_csv(f, sep="\t", header=None, index=False)
+        @type args: subprocess.Namespace
+        @param args: defined arguments
+        """
 
-    log.record(f"gene-to-projection hash with {len(table)} entries written to {f}")
-
-    return f
-
-
-def filter_bed(args: argparse.Namespace, table: pd.DataFrame) -> str:
-    """
-    Filters the original .bed file to produce a custom filtered file
-
-    @type args: subprocess.Namespace
-    @param args: defined arguments
-    @type table: pd.DataFrame
-    @param table: a pandas DataFrame
-    @rtype: str
-    @return path: path to filtered file
-    """
-
-    global log
-
-    initial = len(table)
-
-    if args.threshold:
-        table = table[table["pred"] >= float(args.threshold)]
-        log.record(
-            f"discarded {initial - len(table)} projections with orthology scores <{args.threshold}"
+        self.log.start()
+        self.log.intro()
+        self.log.record(f"postoga started!")
+        self.log.record(
+            f"running in mode {self.mode} with arguments: {vars(self.args)}"
         )
 
-    if args.by_class:
-        edge = len(table)
-        table = table[table["class"].isin(args.by_class.split(","))]
-        log.record(
-            f"discarded {edge - len(table)} projections with classes other than {args.by_class}"
-        )
+        if self.mode != "haplotype":
+            self.table = query_table(self.path)
+            self.isoforms = isoform_writer(self.path, self.table)
 
-    if args.by_rel:
-        edge = len(table)
-        table = table[table["relation"].isin(args.by_rel.split(","))]
-        log.record(
-            f"discarded {edge - len(table)} projections with relationships other than {args.by_rel}"
-        )
+            if any([self.by_class, self.by_rel, self.threshold]):
+                self.bed = self.filter_bed()
+            else:
+                self.bed = os.path.join(self.path, Constants.FileNames.BED)
 
-    # Read the original .bed file and filter it based on the transcripts table
-    bed = pd.read_csv(
-        os.path.join(args.path, Constants.FileNames.BED), sep="\t", header=None
-    )
-    bed = bed[bed[3].isin(table["transcripts"])]
-    custom_table = table[table["transcripts"].isin(bed[3])]
+            if self.to == "gtf":
+                self.gtf = bed_to_gtf(self.path, self.bed, self.isoforms)
+            elif self.to == "gff":
+                self.gff = bed_to_gff(self.path, self.bed, self.isoforms)
 
-    # Write the filtered .bed file
-    f = os.path.join(args.path, Constants.FileNames.FILTERED_BED)
-    bed.to_csv(
-        f,
-        sep="\t",
-        header=None,
-        index=False,
-    )
+            ##### STEP 2 #####
+            qual_by_ancestral(self.path, self.bed, self.table, self.q_assembly)
 
-    log.record(
-        f"kept {len(bed)} projections after filters, discarded {initial - len(bed)}."
-    )
-    log.record(
-        f"{len(bed)} projections are coming from {len(custom_table['helper'].unique())} unique transcripts and {len(custom_table['t_gene'].unique())} genes"
-    )
-    log.record(
-        f"class stats of new bed: {custom_table['class'].value_counts().to_dict()}"
-    )
-    log.record(
-        f"relation stats of new bed: {custom_table['relation'].value_counts().to_dict()}"
-    )
-    log.record(
-        f"confidence stats of new bed: {custom_table['confidence_level'].value_counts().to_dict()}"
-    )
-    log.record(f"filtered bed file written to {f}")
-
-    return f
-
-
-def shell(cmd) -> str:
-    """
-    Run a shell command and return the output as a string
-
-    @type cmd: str
-    @param cmd: shell command
-    """
-    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-    return result.stdout.strip()
-
-
-def bed_to_gtf(bed: str, isoforms: str) -> str:
-    """
-    Converts a .bed file to .gtf
-
-    @type bed: str
-    @param bed: path to .bed file
-    @type isoforms: str
-    @param isoforms: path to the isoforms file
-    """
-
-    global log
-
-    gtf = f"{bed.split('.')[0]}.gtf"
-    cmd = f"{Constants.ToolNames.BED2GTF} {bed} {isoforms} {gtf}"
-    sh = shell(cmd)
-
-    log.record(
-        f"using {Constants.ToolNames.BED2GTF} from {Constants.Metadata.BED2GTF_METADATA} to convert bed to gtf"
-    )
-    log.record(sh)
-    log.record(f"gff file written to {gtf}")
-
-    return gtf
-
-
-def bed_to_gff(bed: str, isoforms: str) -> str:
-    """
-    Converts a .bed file to .gff
-
-    @type bed: str
-    @param bed: path to .bed file
-    @type isoforms: str
-    @param isoforms: path to the isoforms file
-    """
-
-    global log
-
-    gff = f"{bed.split('.')[0]}.gff"
-    cmd = f"{Constants.ToolNames.BED2GFF} {bed} {isoforms} {gff}"
-    sh = shell(cmd)
-
-    log.record(
-        f"using {Constants.ToolNames.BED2GFF} from {Constants.Metadata.BED2GFF_METADATA} to convert bed to gff"
-    )
-    log.record(sh)
-    log.record(f"gff file written to {gff}")
-
-    return gff
-
-
-def run(args: argparse.Namespace) -> None:
-    """
-    The postoga runner function
-
-    @type args: subprocess.Namespace
-    @param args: defined arguments
-    """
-
-    global log
-
-    log = Log(args.path, Constants.FileNames.LOG)
-    log.start()
-    log.record(f"postoga started!")
-    log.record(f"running with arguments: {vars(args)}")
-
-    # Get the toga table and write isoforms
-    table = toga_table(args)
-    isoforms = write_isoforms(args, table)
-
-    # Filtering step (if any)
-    if any([args.by_class, args.by_rel, args.threshold]):
-        bed = filter_bed(args, table)
-    else:
-        bed = os.path.join(args.path, Constants.FileNames.BED)
-
-    # Conversion step
-    if args.to == "gtf":
-        bed_to_gtf(bed, isoforms)
-    elif args.to == "gff":
-        bed_to_gff(bed, isoforms)
-
-    log.close()
-
-    return
+        else:
+            hap_classes = self.get_haplotype_classes()
 
 
 def parser():
     """Argument parser for postoga"""
     app = argparse.ArgumentParser()
+    app.add_argument(
+        "-m",
+        "--mode",
+        help="Run mode",
+        type=str,
+        choices=["default", "haplotype"],
+        default="default",
+    )
     app.add_argument(
         "-p", "--path", help="Path to TOGA results directory", required=True, type=str
     )
@@ -340,18 +219,48 @@ def parser():
         type=str,
         choices=["gtf", "gff"],
     )
+    app.add_argument(
+        "-aq",
+        "--assembly_qual",
+        help="Calculate assembly quality based on a list of genes provided by the user (default: Ancestral_placental.txt)",
+        required=False,
+        type=str,
+        default=Constants.FileNames.ANCESTRAL,
+    )
+    app.add_argument(
+        "-hpath",
+        "--haplotype_path",
+        help="Path to haplotype directory",
+        required=False,
+        type=str,
+    )
+    app.add_argument(
+        "-r",
+        "--rule",
+        help="Rule to merge haplotype assemblies (default: I>PI>UL>L>M>PM>PG>abs)",
+        required=False,
+        type=str,
+        default="I>PI>UL>L>M>PM>PG>abs",
+    )
 
     if len(sys.argv) < 2:
         app.print_help()
         sys.exit(0)
+
     args = app.parse_args()
+
+    if args.mode == "haplotype" and not args.haplotype_path:
+        app.error(
+            "haplotype mode requires the path to the haplotype directory, please provide it with the -hpath/--haplotype_path argument"
+        )
 
     return args
 
 
 def main():
     args = parser()
-    run(args)
+    master = TogaDir(args)
+    master.run()
 
 
 if __name__ == "__main__":
