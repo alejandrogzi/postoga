00e4a2de61dbf70d0a1ea4bcee858499198b1115
alejandrogzi <jose.gonzalesdezavala1@unmsm.edu.pe>
2023-10-07 02:01:15 -0500

    feat(start): start method to be called for the run manager

--- logger.py
+++ logger.py
@@ -16,7 +16,7 @@ from logger import Log
 
 log = Log("path/to/log", "name")
 log.start()
-log.write("This is a message.")
+log.record("This is a message.")
 log.close()
 ```
 
@@ -35,59 +35,68 @@ to better manage the logging of the tool.
 """
 
 
-__author__ = "Alejandro Gonzales-Irribarren"
-__email__ = "jose.gonzalesdezavala1@unmsm.edu.pe"
-__github__ = "https://github.com/alejandrogzi"
-
-
 import os
 import datetime
 from version import __version__
 from constants import Constants
-import subprocess
+from modules.utils import shell
+import logging
+
+
+__author__ = "Alejandro Gonzales-Irribarren"
+__email__ = "jose.gonzalesdezavala1@unmsm.edu.pe"
+__github__ = "https://github.com/alejandrogzi"
+__version__ = "0.4.0-devel"
 
 
 class Log:
     """Logger class for postoga."""
 
-    def __init__(self, path, name):
-        self.path = path
-        self.name = name
-        self.log_file = os.path.join(path, name)
+    def __init__(self, path: str, log_file: str):
+        self.log_file = os.path.join(path, log_file)
         self.version = __version__
-        self.commit = self.shell(Constants.Commands.COMMIT)
-        self.branch = self.shell(Constants.Commands.BRANCH)
+        self.commit = shell(Constants.Commands.COMMIT)
+        self.branch = shell(Constants.Commands.BRANCH)
 
     def start(self):
+        logging.basicConfig(
+            filename=self.log_file,
+            level=logging.INFO,
+            format="[%(asctime)s] - %(levelname)s: %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+
+    def intro(self):
         start_message = f"{'#'*36}\npostoga: the post-TOGA processing pipeline"
         version = f"version: {self.version}"
         commit = f"commit: {self.commit}"
-        branch = f"branch: {self.branch}"
+        branch = f"branch: {self.branch}\n\n"
         metadata = "\n".join([version, commit, branch])
+
         with open(self.log_file, "w") as log:
-            log.write(f"{start_message}\n\n{metadata}\n\n")
+            log.write("\n".join([start_message, metadata]))
 
     def record(self, message, timestamp=True):
-        formatted_message = message
-        if timestamp:
-            timestamp = datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
-            formatted_message = f"{timestamp} - {message}"
+        logging.info(message)
 
-        with open(self.log_file, "a") as log:
-            log.write(f"{formatted_message}\n")
+    @classmethod
+    def connect(cls, path, log_file):
+        log = cls(path, log_file)
+        log.start()
+        return log
 
     def close(self):
         end_message = f"postoga finished!\n{'#'*36}"
-        with open(self.log_file, "a") as log:
-            log.write(f"\n{end_message}\n")
-
-    def shell(self, cmd):
-        result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-        return result.stdout.strip()
+        logging.info(end_message)
 
 
 if __name__ == "__main__":
-    log = Log(".", "test")
+    # To create a new log file
+    log = Log("path/to/log", "name")
     log.start()
-    log.write("This is a message.")
-    log.close()
+    log.record("This is a message.")
+    log.record("Another message.")
+
+    # To use an existing log file
+    log = Log.connect("my_existing_log.log")
+    log.record("Log this message in the existing log.")
