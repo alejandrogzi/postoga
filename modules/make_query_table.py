afc2e1a83e8c62367bc96d47ffe7610d8368d2ea
alejandrogzi <jose.gonzalesdezavala1@unmsm.edu.pe>
2024-04-08 23:10:18 -0500

    feat: inner join -> left join; issue #6

--- modules/make_query_table.py
+++ modules/make_query_table.py
@@ -57,18 +57,17 @@ def query_table(path: str) -> pd.DataFrame:
     score["transcripts"] = [f"{k}.{v}" for k,v in zip(score["gene"], score["chain"])]
     score = score[["transcripts", "pred", "gene"]]
 
-    table = pd.merge(ortho_x_loss, score, left_on="transcript", right_on="transcripts")
+    table = pd.merge(ortho_x_loss, score, left_on="transcript", right_on="transcripts", how="left")
 
     # Create a new column with a rename orthology relationship
     table["relation"] = table["orthology_class"].map(Constants.ORTHOLOGY_TYPE)
     table["t_gene"].fillna(table["helper"].map(isoforms_dict), inplace=True)
 
-    # Add paralog probabilities
-    paralog = pd.merge(score, paralogs, on="transcripts").groupby("gene").agg({"pred": "max"}).reset_index()
-    paralog.columns = ["helper", "paralog_prob"]
-
-    table = pd.merge(table, paralog, on="helper", how="left")
-    table["paralog_prob"].fillna(0, inplace=True)
+    # Add paralog probabilities -> disabled for now 
+    # paralog = pd.merge(score, paralogs, on="transcripts").groupby("gene").agg({"pred": "max"}).reset_index()
+    # paralog.columns = ["helper", "paralog_prob"]
+    # table = pd.merge(table, paralog, on="helper", how="left")
+    # table["paralog_prob"].fillna(0, inplace=True)
 
     # # Merge quality data
     # table = pd.merge(table, quality, left_on="transcripts", right_on="Projection_ID")
