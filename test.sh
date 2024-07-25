6bdcb2b88ec18798f3cea23a275e9f462eb9640d
alejandrogzi <jose.gonzalesdezavala1@unmsm.edu.pe>
2024-07-24 23:58:57 -0500

    fix: rm OUTDIR after test finishes

--- test.sh
+++ test.sh
@@ -7,9 +7,12 @@ OUTDIR="./test_out"
 
 
 if [[ -d $TESTDIR ]]; then
-    ./postoga.py $MODE --td $TESTDIR -o $OUTDIR --to gff -th 0.5
+    ./postoga.py $MODE --td $TESTDIR -o $OUTDIR --to gff -th 0.5 --skip
+    wait
+    echo "Test completed!"
 else
     echo "Directory $TESTDIR not found, clone the repository again or contact the developer"
 fi
 
 head -n 9 $LOG
+rm -rf $OUTDIR
