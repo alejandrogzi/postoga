acce2ec013ca0caea8a5493f26266403a2250e88
alejandrogzi <alejandrxgzi@gmail.com>
2024-10-15 12:39:13 +0200

    BREAKING CHANGE: migrate configuration and testing to pyproject + Makefile

---
+++ Makefile
@@ -0,0 +1,48 @@
+# Makefile
+
+# author = "Alejandro Gonzales-Irribarren"
+# email = "alejandrxgzi@gmail.com"
+# github = "https://github.com/alejandrogzi"
+# version: "0.9.3-devel"
+
+.PHONY: configure test create --conda
+
+PYTHON_SCRIPT=postoga_test.py
+PROJECT := $(CURDIR)/POSTOGA_TEST
+CONFIG=configure.sh
+VERSION=0.9.3-devel
+TEST_PATH := $(CURDIR)/supply/test
+THRESHOLD := 0.95
+CLASS := I,PI
+FMT := gtf
+
+configure:
+	@echo ""
+	@echo "> postoga v.$(VERSION) configuration!"
+	@echo ">> this may take some minutes..."
+	@echo ""
+	$(eval CONDA_FLAG := $(filter --conda,$(MAKECMDGOALS)))
+	bash $(CONFIG) $(CONDA_FLAG)
+
+--conda:
+	@:
+
+create:
+	@echo "#!/usr/bin/env python3" > $(PYTHON_SCRIPT)
+	@echo "from run import parser, TogaDir" >> $(PYTHON_SCRIPT)
+	@echo "" >> $(PYTHON_SCRIPT)
+	@echo "def main():" >> $(PYTHON_SCRIPT)
+	@echo "    args = parser()" >> $(PYTHON_SCRIPT)
+	@echo "    TogaDir(args).run()" >> $(PYTHON_SCRIPT)
+	@echo "" >> $(PYTHON_SCRIPT)
+	@echo "if __name__ == \"__main__\":" >> $(PYTHON_SCRIPT)
+	@echo "    main()" >> $(PYTHON_SCRIPT)
+
+
+test: create
+	@python3 $(PYTHON_SCRIPT) base --togadir $(TEST_PATH) --outdir $(PROJECT) -th $(THRESHOLD) -bc $(CLASS) -to $(FMT) || (echo "> ERROR: test failed!" && exit 1)
+	@rm $(PYTHON_SCRIPT)
+	@mv $(PROJECT)'/postoga.log' './_postoga.test.log'
+	@rm -r $(PROJECT)
+	@echo ""
+	@echo "> test completed!"
--- configure.sh
+++ configure.sh
@@ -1,37 +1,103 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# author = "Alejandro Gonzales-Irribarren"
+# email = "alejandrxgzi@gmail.com"
+# github = "https://github.com/alejandrogzi"
+# version: 0.9.3-devel
 
 
-# author: Alejandro Gonzales-Irribarren  
-# email: jose.gonzalesdezavala1@unmsm.edu.pe
-# github: https://github.com/alejandrogzi
+TEST_PY="postoga_test.py"
+TEST_PROJECT="test_project"
+ENV_YML="env.yml"
+PISCO_ENV="postoga"
+CONDA_FLAG=false
 
+echo ""
+echo ">> configuring environment to run postoga"
 
-GET_BINARIES=true
-INSTALL_PYTHON=false
-
-if $INSTALL_PYTHON && [[ -f "./modules/requirements.py" ]]; then
-    echo "Installing Python dependencies..."
-    ./modules/requirements.py
-fi
-
-
-if command -v cargo &> /dev/null; then
-    if $GET_BINARIES; then
-        echo "Installing Rust binaries..."
-        cat ./modules/rust-binaries.txt | xargs -n 1 cargo install
-    else
-        if [[ ! -f "./bed2gtf/Cargo.toml" ]] || [[ ! -f "./bed2gff/Cargo.toml" ]] || [[ ! -f "./noel/Cargo.toml" ]]; then
-            git submodule init bed2gtf bed2gff noel && git submodule update bed2gtf bed2gff noel
-        fi
-            echo "Building bed2gtf..."
-            cd ./bed2gtf && cargo build --release && cargo install --path .
-
-            echo "Building bed2gff..."
-            cd ../bed2gff && cargo build --release && cargo install --path .
-
-            echo "Building noel..."
-            cd ../noel && cargo build --release && cargo install --path .
+for arg in "$@"
+do
+    if [ "$arg" == "--conda" ]; then
+        CONDA_FLAG=true
+        break
     fi
+done
+
+
+if $CONDA_FLAG; then
+  echo ">> using conda to create environment..."
+
+  if ! command -v conda &> /dev/null
+  then
+      echo "!ERROR: conda could not be found, please install Miniconda or Anaconda."
+      exit
+  fi
+
+  if [ -f $ENV_YML ]; then
+      echo ">> creating conda environment..." && echo ""
+      conda env create -f $ENV_YML
+
+      if [[ $? -eq 0 ]]; then
+          sleep 3
+          eval "$(conda shell.bash hook)"
+          conda activate $PISCO_ENV
+      else
+          echo ">> conda env create failed, activating existing environment..."
+          sleep 3
+          eval "$(conda shell.bash hook)"
+          conda activate $PISCO_ENV
+      fi
+  else
+      echo "!ERROR: $ENV_YML not found. Please clone the repository again!"
+      exit
+  fi
 else
-    echo "cargo not found, please install Rust"
+  echo ">> using hatch to create environment..."
+
+  hatch env create
+
+  if [[ $? -ne 0 ]]; then
+      echo "!ERROR: hatch env create failed. Please check the error message above."
+      exit 1
+  fi
+
+  VENV_PATH=$(hatch env find)
+  if [[ $? -ne 0 ]]; then
+      echo "!ERROR: Failed to find the hatch environment."
+      exit 1
+  fi
+
+  source "$VENV_PATH/bin/activate"
+  if [[ $? -ne 0 ]]; then
+      echo "!ERROR: Failed to activate the hatch environment."
+      exit 1
+  fi
+
+  echo ">> environment created successfully!"
 fi
+
+echo ">> building rust extensions and binaries..."
+cd rustools && maturin develop --release
+cd ..
+cargo install bed2gtf --force
+cargo install bed2gff --force
+cargo install gxf2bed --force
+echo ">> rust extensions and binaries built successfully!"
+
+echo "" && echo ">> do you want to run the test? [y/n]"
+read -r run_test
+
+if [ "$run_test" == "y" ]; then
+    echo ">> running test..."
+    make test
+
+    if [[ $? -ne 0 ]]; then
+        echo "!ERROR: test failed. Please check the error message above."
+        exit
+    fi
+
+    # clean up
+    rm -f $TEST_PY && rm -rf $TEST_PROJECT
+fi
+
+echo "" && echo ">> SUCCESS: environment has been configured successfully. You are now ready to use postoga!"
